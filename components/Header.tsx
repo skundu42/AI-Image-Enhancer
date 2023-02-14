73da72e60df524bddb2ad9300ed509b1872be71a
skundu42 <skundu_be19@thapar.edu>
2023-02-14 21:33:07 +0530

    initial commit

---
+++ components/Header.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header className="flex justify-between items-center w-full mt-5 border-b-2 pb-7 sm:px-4 px-2">
+      <Link href="/" className="flex space-x-2">
+        <Image
+          alt="header text"
+          src="/imageIcon.png"
+          className="sm:w-14 sm:h-14 w-9 h-9"
+          width={36}
+          height={36}
+        />
+        <h1 className="sm:text-5xl text-3xl font-bold ml-2 tracking-tight">
+          Unblur.io
+        </h1>
+      </Link>
+    </header>
+  );
+}
